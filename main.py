dad23b8f1495801b4fd2fbd13e702a50430bac1f
arsonwong <arsonwong@gmail.com>
2024-12-15 03:16:28 +0000

    added create_subcell based on sought Voc, FF, Isc

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
 import numpy as np
-from tandem_cells.diodes import Stack, SubCell, Diode
+from tandem_cells.diodes import Stack, SubCell, Diode, create_subcell
 
-Vs = np.arange(0, 0.7 + 0.1, 0.1)
-
-diode = Diode()
-I = diode.solve_I(Vs)
+top_cell = create_subcell(21e-3, 1.3, 0.78, 1, 2, 1)
+bottom_cell = create_subcell(20e-3, 0.7, 0.78, 1, 2, 1)
+tandem_cell = Stack([top_cell,bottom_cell])
+tandem_cell.tabulate_subcells(V=None)
+V = np.linspace(0,2.2,100)
+I = tandem_cell.solve_I(V)
 print(I)
-
-subcell = SubCell([Diode()]*2)
-subcell.solve_I(np.arange(0, 0.7 + 0.1, 0.1),tabulate=True)
-s = Stack([subcell]*8)
-
-print(s)
-
-s.tabulate_subcells(Vs)
-print(s.I_range)
-print(s.solve_V(-1* 10.0 ** np.arange(-3, -15, -1)))
-
-print(s.IV_table)
-
-print(s.solve_V(-1e-4))
-
-print(s.solve_I(3))
--- tandem_cells/diodes.py
+++ tandem_cells/diodes.py
@@ -1,21 +1,24 @@
 import numpy as np
+from matplotlib import pyplot as plt
 
 VT = 0.02568
 
 class Diode:
-    def __init__(self,I0=1e-15,n=1):
+    def __init__(self,I0=1e-15,n=1,V_shift=0): #V_shift is to shift the starting voltage, e.g. to define breakdown
         self.I0 = I0 #A
         self.n = n
         self.IL = 0
+        self.V_shift = V_shift
     def __str__(self):
         return "I0 = " + str(self.I0) + " A, n = " + str(self.n)
     def solve_I(self, V, VT = VT):
-        return self.I0*np.exp(V/(self.n*VT))
+        return self.I0*np.exp((V-self.V_shift)/(self.n*VT))
 
 class SubCell:
-    def __init__(self,diodes=[Diode()],IL = 0, shunt_cond=0,name="subcell"):
+    def __init__(self,diodes=[Diode()],rev_diodes=[],IL = 0, shunt_cond=0,name="subcell"):
         self.N_diodes = len(diodes)
         self.diodes = diodes
+        self.rev_diodes = rev_diodes
         for i, diode in enumerate(diodes):
             if not isinstance(diode,Diode):
                 self.diodes[i] = Diode()
@@ -35,11 +38,13 @@ class SubCell:
         I = self.IL - V*self.shunt_cond
         for diode in self.diodes:
             I -= diode.solve_I(V,VT=VT) 
+        for diode in self.rev_diodes:
+            I += diode.solve_I(-V,VT=VT) 
         if tabulate:
             indices = np.argsort(I)
-            V = V[indices]
-            I = I[indices]     
-            self.IV_table = np.vstack((V,I))  
+            V_ = V[indices]
+            I_ = I[indices]     
+            self.IV_table = np.vstack((V_,I_))  
         return I
 
 class Stack:
@@ -60,13 +65,14 @@ class Stack:
             word += str(subcell)
         return word
     def tabulate_subcells(self, V, VT = VT):
-        for i, _ in enumerate(self.subcells):
-            I = self.subcells[i].solve_I(V, VT = VT, tabulate=True)
+        for i, cell in enumerate(self.subcells):
+            if cell.IV_table is None:
+                cell.solve_I(V, VT = VT, tabulate=True)
             if i==0:
-                self.I_range = [np.min(I),np.max(I)]
+                self.I_range = [np.min(cell.IV_table[1,:]),np.max(cell.IV_table[1,:])]
             else:
-                self.I_range[0] = np.max([self.I_range[0],np.min(I)])
-                self.I_range[1] = np.min([self.I_range[1],np.max(I)])
+                self.I_range[0] = np.max([self.I_range[0],np.min(cell.IV_table[1,:])])
+                self.I_range[1] = np.min([self.I_range[1],np.max(cell.IV_table[1,:])])
         if self.I_range[0] < self.I_range[1]: #tabulate the two extreme points
             self.solve_V(np.array(self.I_range))
     def solve_V(self,I):
@@ -93,9 +99,40 @@ class Stack:
         while True:
             I[indices] = np.interp(V[indices], self.IV_table[0,:], self.IV_table[1,:])
             Vnew = self.solve_V(I[indices])
+            print(np.max(np.abs(Vnew-V[indices])))
             if np.max(np.abs(Vnew-V[indices])) < V_tolerance:
                 break
         return I
+    
+def create_subcell(Isc, Voc, FF, n1, n2, n_rev, I0_rev=1e-15, breakdown_V=-10, VT=VT, name="subcell"):
+    max_power = Isc*Voc*FF
+    cell = SubCell(diodes=[Diode(n=n1),Diode(n=n2)],rev_diodes=[Diode(n=n_rev,V_shift=-breakdown_V)],IL = Isc, shunt_cond=0,name=name)
+    lower_V = -VT*np.log(Isc/I0_rev)+breakdown_V
+    V = np.hstack((np.linspace(lower_V,breakdown_V,100), np.linspace(breakdown_V*0.99,breakdown_V*0.01,100), np.linspace(0, 2*Voc, 200)))
+    # rough guess to start
+    cell.diodes[0].I0 = Isc/np.exp(Voc/(n1*VT))
+    cell.diodes[1].I0 = 0
+    for i in range(100):
+        I = cell.solve_I(V, tabulate=True)
+        power = V*I
+        Voc_ = np.interp(0.0, cell.IV_table[1,:], cell.IV_table[0,:])
+        mpp = np.argmax(power)
+        Vmp = V[mpp]
+        max_power_ = power[mpp]
+        denom = cell.diodes[0].solve_I(Voc_)/(n1*VT) + cell.diodes[1].solve_I(Voc_)/(n2*VT)
+        dVoc_dI01 = -np.exp(Voc_/(n1*VT))/denom
+        dVoc_dI02 = -np.exp(Voc_/(n2*VT))/denom
+        dpower_dI01 = -Vmp*np.exp(Vmp/(n1*VT))
+        dpower_dI02 = -Vmp*np.exp(Vmp/(n2*VT))
+        M = np.array([[dVoc_dI01, dVoc_dI02],[dpower_dI01, dpower_dI02]])
+        Y = np.array([Voc - Voc_, max_power-max_power_])
+        x = np.linalg.solve(M, Y)
+        cell.diodes[0].I0 += x[0]
+        cell.diodes[1].I0 += x[1]
+        if np.max(np.abs(Y))<1e-10:
+            break
+    return cell
+
 
     
 
